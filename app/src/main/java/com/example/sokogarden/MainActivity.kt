dec00502523d033da16e544a2372bd0880704c7d
vanessa-wambui <vanessawambui251@gmail.com>
2026-04-15 11:24:06 +0300

    add new codes

--- app/src/main/java/com/example/sokogarden/MainActivity.kt
+++ app/src/main/java/com/example/sokogarden/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.sokogarden
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.IntentSenderRequest
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +35,27 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, SigninActivity::class.java)
             startActivity(intent)
         }
+//        find the recycler view and progress bar by use of their ids
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+//        specify the Url
+        val url = "https://kbenkamotho.alwaysdata.net/api/get_products"
+
+//        import the helper
+
+        val helper = ApiHelper(applicationContext)
+
+
+//        inside of the helper class access the function load products
+
+        helper.loadProducts(url,recyclerView, progressBar)
+
+
+
+
+
+//
+
 
 
     }
--- app/src/main/java/com/example/sokogarden/SignupActivity.kt
+++ app/src/main/java/com/example/sokogarden/SignupActivity.kt
@@ -1,10 +1,15 @@
 package com.example.sokogarden
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.loopj.android.http.RequestParams
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,46 @@ class SignupActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+// find all view by use of their ids
+        val username = findViewById<EditText>(R.id.username)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(R.id.password)
+        val phone = findViewById<EditText>(R.id.phone)
+        val signupButton = findViewById<Button>(R.id.signupBtn)
+        val signinTextView = findViewById<TextView>(R.id.signintxt)
+//below when a person clicks on the TextView, he/she is navigated to the singin page
+        signinTextView.setOnClickListener {
+            val intent = Intent(applicationContext, SigninActivity::class.java)
+            startActivity(intent)
+        }
+
+//on click of the signup Button, we want to register a person
+        signupButton.setOnClickListener {
+            // Specify the API endpoint
+            val api = "https://kbenkamotho.alwaysdata.net/api/signup"
+
+            // Create a RequestParams ~ it is where we are going to hold all the data
+            val data = RequestParams()
+
+            // Add/Append the username, email, password and phone on the data
+            data.put("username", username.text.toString().trim())
+            data.put("email", email.text.toString().trim())
+            data.put("password", password.text.toString().trim())
+            data.put("phone", phone.text.toString().trim())
+
+//            import the API helper
+            val helper = ApiHelper(applicationContext)
+
+//            inside of the helper  access the function post
+            helper.post(api, data)
+
+            email.text.clear()
+            password.text.clear()
+            phone.text.clear()
+            username.text.clear()
+        }
+
+        }
+
     }
-}
+
